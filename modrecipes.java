f60f20d8a2b636ce0592919173f8646b9f44bec8
Nico <nico.mexis@kabelmail.de>
2016-08-04 12:27:09 +0200

    Some cleanup
    No need to create new Object-Arrays

--- modrecipes.java
+++ modrecipes.java
@@ -9,17 +9,14 @@ public class modrecipes
 
 {
 
-    {
-        GameRegistry.addRecipe(new ItemStack(Items.NETHER_STAR),new Object[]{ "nqs", "qeq", "sqn", 'n', Blocks.NETHERRACK,'s', Blocks.SOUL_SAND,'e', Blocks.EMERALD_BLOCK,'q', Items.QUARTZ});
-        GameRegistry.addRecipe(new ItemStack(Blocks.LOG),new Object[]{"DD","DD","",'D', new ItemStack(Blocks.DIRT,4)});
-        GameRegistry.addRecipe(new ItemStack(Blocks.STONE), new Object[]{"ll","ll","",'l', new ItemStack(Blocks.LOG,4)});
-        GameRegistry.addRecipe(new ItemStack(Blocks.IRON_BLOCK), new Object[]{"ss","ss","",'s', new ItemStack(Blocks.STONEBRICK,4)});
-        GameRegistry.addRecipe(new ItemStack(Blocks.GOLD_BLOCK), new Object[]{ "ii","ii","",'i', new ItemStack(Blocks.IRON_BLOCK,4)});
-        GameRegistry.addRecipe(new ItemStack(Blocks.DIAMOND_BLOCK), new Object[]{"gg","gg","",'g', new ItemStack(Blocks.GOLD_BLOCK,4)});
-        GameRegistry.addRecipe(new ItemStack(Blocks.EMERALD_BLOCK), new Object[]{ "dd ","dd",'d',new ItemStack(Blocks.DIAMOND_BLOCK,4)});
-        GameRegistry.addRecipe(new ItemStack(Blocks.COAL_BLOCK), new Object[]{"cc","cc","",'c', new ItemStack(Blocks.COBBLESTONE)});
-    }
-
     public static void init() {
+        GameRegistry.addRecipe(new ItemStack(Items.NETHER_STAR), "nqs", "qeq", "sqn", 'n', Blocks.NETHERRACK,'s', Blocks.SOUL_SAND,'e', Blocks.EMERALD_BLOCK,'q', Items.QUARTZ);
+        GameRegistry.addRecipe(new ItemStack(Blocks.LOG), "DD","DD","",'D', new ItemStack(Blocks.DIRT,4));
+        GameRegistry.addRecipe(new ItemStack(Blocks.STONE), "ll","ll","",'l', new ItemStack(Blocks.LOG,4));
+        GameRegistry.addRecipe(new ItemStack(Blocks.IRON_BLOCK), "ss","ss","",'s', new ItemStack(Blocks.STONEBRICK,4));
+        GameRegistry.addRecipe(new ItemStack(Blocks.GOLD_BLOCK), "ii","ii","",'i', new ItemStack(Blocks.IRON_BLOCK,4));
+        GameRegistry.addRecipe(new ItemStack(Blocks.DIAMOND_BLOCK), "gg","gg","",'g', new ItemStack(Blocks.GOLD_BLOCK,4));
+        GameRegistry.addRecipe(new ItemStack(Blocks.EMERALD_BLOCK), "dd ","dd",'d',new ItemStack(Blocks.DIAMOND_BLOCK,4));
+        GameRegistry.addRecipe(new ItemStack(Blocks.COAL_BLOCK), "cc","cc","",'c', new ItemStack(Blocks.COBBLESTONE));
     }
 }
